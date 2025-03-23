97cbb25c7f6a33eda814c13bca51e28dbd989eae
kimdev <kim__dev@hufs.ac.kr>
2025-03-23 22:40:31 +0900

    refactor: Byeong의 이동 기능 리팩토링

---
+++ src/main/java/model/piece/Byeong.java
@@ -0,0 +1,40 @@
+package model.piece;
+
+import model.position.Position;
+
+import java.util.List;
+
+public class Byeong extends Piece {
+
+    public Byeong(final Position position) {
+        super(position);
+    }
+
+    @Override
+    public PieceType getPieceType() {
+        return PieceType.BYEONG;
+    }
+
+    @Override
+    public List<Position> calculateRouteToDestination(final Position destination) {
+        if (isPieceCanGo(destination)) {
+            return findRouteToDestination(destination);
+        }
+        throw new IllegalArgumentException("현재 기물이 이동할 수 없는 위치입니다.");
+    }
+
+    private boolean isPieceCanGo(final Position destination) {
+        int rowDifference = destination.calculateRowDifference(position);
+        int columnDifference = destination.calculateColumnDifference(position);
+        return (rowDifference == 0 || rowDifference == 1) && Math.abs(columnDifference) <= 1;
+    }
+
+    private List<Position> findRouteToDestination(final Position destination) {
+        int rowStep = destination.calculateRowDifference(position);
+        int columnStep = destination.calculateColumnDifference(position);
+        if (position.canChangeOfRowAndColumn(rowStep, columnStep)) {
+            return List.of(position.changeRowAndColumn(rowStep, columnStep));
+        }
+        throw new IllegalArgumentException("10x9 범위를 벗어났습니다.");
+    }
+}
--- src/test/java/model/ByeongTest.java
+++ src/test/java/model/ByeongTest.java
@@ -2,37 +2,63 @@ package model;
 
 import java.util.List;
 
+import model.piece.Byeong;
+import model.piece.Piece;
+import model.piece.PieceType;
 import model.position.Position;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ByeongTest {
 
-    @DisplayName("이동 가능한 모든 위치를 가져온다")
-    @Test
-    void calculate_all_direction() {
-        Byeong byeong = new Byeong(new Position(5, 5));
-        List<List<Position>> moveResults = byeong.calculateAllDirection();
-        List<List<Position>> expected = List.of(
-            List.of(new Position(6, 5)),
-            List.of(new Position(5, 4)),
-            List.of(new Position(5, 6))
-        );
-        assertThat(moveResults).isEqualTo(expected);
+    private Piece byeong;
+
+    @BeforeEach
+    void init() {
+        byeong = new Byeong(new Position(5, 5));
     }
 
-    @DisplayName("0~9행, 0~8열을 벗어나면 빈 리스트를 반환해야 한다")
     @Test
-    void invalid_direction_calculation_then_empty_list() {
-        Byeong byeong = new Byeong(new Position(9, 2));
-        List<List<Position>> moveResults = byeong.calculateAllDirection();
-        List<List<Position>> expected = List.of(
-            List.of(),
-            List.of(new Position(9, 1)),
-            List.of(new Position(9, 3))
-        );
-        assertThat(moveResults).isEqualTo(expected);
+    void 병_기물의_PieceType을_반환한다() {
+        // Given
+        // When & Then
+        assertThat(byeong.getPieceType()).isEqualTo(PieceType.BYEONG);
+    }
+
+    @Test
+    void 병_기물이_수평_방향으로_움직일_수_있는_위치로_가는_경로를_계산한다() {
+        // Given
+        Position destination = new Position(5, 4);
+        Position destination2 = new Position(5, 6);
+
+        // When & Then
+        assertThat(byeong.calculateRouteToDestination(destination))
+                .isEqualTo(List.of(new Position(5, 4)));
+        assertThat(byeong.calculateRouteToDestination(destination2))
+                .isEqualTo(List.of(new Position(5, 6)));
+    }
+
+    @Test
+    void 병_기물이_수직_방향으로_움직일_수_있는_위치로_가는_경로를_계산한다() {
+        // Given
+        Position destination2 = new Position(6, 5);
+
+        // When & Then
+        assertThat(byeong.calculateRouteToDestination(destination2))
+                .isEqualTo(List.of(new Position(6, 5)));
+    }
+
+    @Test
+    void 병_기물이_이동할_수_없는_위치로는_경로를_계산할_수_없다() {
+        // Given
+        Position invalidDestination = new Position(7, 7);
+
+        // When & Then
+        assertThatThrownBy(() -> byeong.calculateRouteToDestination(invalidDestination))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("현재 기물이 이동할 수 없는 위치입니다.");
     }
 }
