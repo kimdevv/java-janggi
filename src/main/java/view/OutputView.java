7a940c7ff6e8fca62cd24fa73fdf3671d8cc4544
kimdev <kim__dev@hufs.ac.kr>
2025-03-27 15:15:42 +0900

    refactor: 궁성을 나타내는 출력 메서드 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,6 +8,12 @@ import model.piece.position.Position;
 import java.util.Arrays;
 
 import static model.piece.position.Position.COLUMN_COUNT_OF_JANGGI;
+import static model.piece.position.Position.MAXIMUM_GREEN_PALACE_ROW;
+import static model.piece.position.Position.MAXIMUM_PALACE_COLUMN;
+import static model.piece.position.Position.MAXIMUM_RED_PALACE_ROW;
+import static model.piece.position.Position.MINIMUM_GREEN_PALACE_ROW;
+import static model.piece.position.Position.MINIMUM_PALACE_COLUMN;
+import static model.piece.position.Position.MINIMUM_RED_PALACE_ROW;
 import static model.piece.position.Position.ROW_COUNT_OF_JANGGI;
 
 public class OutputView {
@@ -33,7 +39,19 @@ public class OutputView {
         for (String[] piecePositionText : piecePositionTexts) {
             Arrays.fill(piecePositionText, EMPTY_POSITION_TEXT);
         }
+        paintPalace(piecePositionTexts);
         return piecePositionTexts;
+    }
+
+    private void paintPalace(final String[][] piecePositionTexts) {
+        for (int column=MINIMUM_PALACE_COLUMN; column<=MAXIMUM_PALACE_COLUMN; column++) {
+            for (int row=MINIMUM_RED_PALACE_ROW; row<=MAXIMUM_RED_PALACE_ROW; row++) {
+                piecePositionTexts[row][column] = "＋";
+            }
+            for (int row=MINIMUM_GREEN_PALACE_ROW; row<=MAXIMUM_GREEN_PALACE_ROW; row++) {
+                piecePositionTexts[row][column] = "＋";
+            }
+        }
     }
 
     private void placePieceTexts(final String[][] piecePositionTexts, final Pieces redPieces, final Pieces greenPieces) {
